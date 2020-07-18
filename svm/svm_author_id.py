2c591ae1575b57479d78f4c7dc2a4b35ac872632
Jonas Wittmann <jonas.wittmann@tum.de>
2020-07-18 20:08:11 +0200

    Fix files and add SVM solution

--- svm/svm_author_id.py
+++ svm/svm_author_id.py
@@ -7,9 +7,12 @@
     Sara has label 0
     Chris has label 1
 """
-    
+
+
 import sys
 from time import time
+
+
 sys.path.append("../tools/")
 from email_preprocess import preprocess
 
@@ -23,8 +26,25 @@ features_train, features_test, labels_train, labels_test = preprocess()
 
 
 #########################################################
-### your code goes here ###
+from sklearn import svm
+clf = svm.SVC(kernel='rbf', C = 10000)
+t0 = time()
+clf.fit(features_train,labels_train)
+print("training time:", round(time()-t0, 3), "s")
 
+t0 =time()
+pred =clf.predict(features_test)
+counter = 0
+for element in pred:
+    if element == 1:
+        counter += 1
+print(counter)
+
+#
+# from sklearn.metrics import accuracy_score
+# accuracy =accuracy_score(pred,labels_test)
+#
+# print accuracy
 #########################################################
 
 
